958b4ec1e585f1eb5de1d0c3b3c68537ace288c2
daniel.gandiaga <daniel.gandiaga@dive.tech>
2021-12-14 22:42:03 +0100

    last version of the training script

--- src/train.py
+++ src/train.py
@@ -6,60 +6,75 @@ import matplotlib.pyplot as plt
 import datetime
 import time
 
-from environment import Env
-from agents import Agent
+from lib.environment import Env
+from lib.agents import Agent
 
 
 parser = argparse.ArgumentParser(description='Train a PPO agent for the CarRacing-v0')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
-parser.add_argument('--action-repeat', type=int, default=8, metavar='N', help='repeat action in N frames (default: 8)')
-parser.add_argument('--img-stack', type=int, default=4, metavar='N', help='stack N image in a state (default: 4)')
-parser.add_argument('--seed', type=int, default=0, metavar='N', help='random seed (default: 0)')
 parser.add_argument('--render', action='store_true', help='render the environment')
-parser.add_argument('--tag', type=str, default='', help='tag for saving results')
-parser.add_argument('--img-size', type=int, default=96, help='image size')
-parser.add_argument(
-    '--log-interval', type=int, default=10, metavar='N', help='interval between training status logs (default: 10)')
+parser.add_argument('--model', type=str, choices=['convnet', 'coordconvnet'], default='coorconvnet',
+                    help='model architecture')
 args = parser.parse_args()
 
 
 if __name__ == "__main__":
-    agent = Agent(args.img_stack, args.img_size, args.gamma, args.seed)
-    n_params = sum(p.numel() for p in agent.net.parameters())
-    env = Env(args.img_size, args.img_stack, args.action_repeat, args.seed)
 
-    date = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    # Agent/Environment initialization
+    agent = Agent(args.model)
+    env = Env(args.model)
+
+    # Variables for logging purposes and metrics
+    date = datetime.datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
+    model_name = f'{args.model}_{date}'
+    n_params = sum(p.numel() for p in agent.net.parameters())
     starting_time = time.time()
     training_records = []
     episode_rewards = []
     accumulated_reward = []
-    results = pd.DataFrame(columns=['time', 'reward', 'avg_reward', 'n_params'])
+    results = pd.DataFrame(columns=['model', 'date', 'episode', 'time', 'reward', 'avg_reward', 'n_params'])
+    best_acc_reward = -100
 
+    # 2000 epochs set as experimentation set-up
     for i_ep in range(2000):
         episode_rewards.append(0)
         state = env.reset()
 
+        # Episode initialization
         for t in range(1000):
+
+            # Action selection
             action, a_logp = agent.select_action(state)
+
+            # Environment step
             state_, reward, done, die = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
+
+            # Render
             if args.render:
                 env.render()
+
+            # Store and update the model if the batch limit is reached
             if agent.store((state, action, a_logp, reward, state_)):
-                print('updating')
+                print('Updating policy...')
                 agent.update()
+
+            # Store reward and update state
             episode_rewards[-1] += reward
             state = state_
             if done or die:
                 break
+
+        # Store accumulated rewards and results, and save them to disk
         if len(episode_rewards) < 100:
             accumulated_reward.append(np.mean(episode_rewards))
         else:
             accumulated_reward.append(np.mean(episode_rewards[-100:]))
 
-        results.loc[len(results)] = [time.time()-starting_time, episode_rewards[-1], accumulated_reward[-1], n_params]
-        results.to_csv(f'results/individual/{args.tag}_{date}.csv', index=False)
+        results.loc[len(results)] = [args.model, date, i_ep, time.time()-starting_time, episode_rewards[-1],
+                                     accumulated_reward[-1], n_params]
+        results.to_csv(f'results/individual/{model_name}.csv', index=False)
 
-        if i_ep % args.log_interval == 0:
+        # Updating visualization and saving model to disk every 10 episodes
+        if i_ep % 10 == 0:
             plt.figure()
             plt.plot(episode_rewards, label='rewards')
             plt.plot(accumulated_reward, label='averaged_reward_over_100_runs')
@@ -67,7 +82,12 @@ if __name__ == "__main__":
             plt.xlabel('Episode')
             plt.ylabel('Reward')
             plt.legend()
-            plt.savefig(f'results/individual/{args.tag}_{date}.png')
+            plt.savefig(f'results/individual/{model_name}.png')
+            plt.close()
             print('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(i_ep, episode_rewards[-1],
                                                                                    accumulated_reward[-1]))
-            agent.save_param(args.tag, date)
+
+            if best_acc_reward < accumulated_reward[-1]:
+                agent.save_param(model_name)
+                best_acc_reward = accumulated_reward[-1]
+                print('Saving model into disk...')
